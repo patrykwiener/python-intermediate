17d6735c6a0fcc2c15f432091391fde79832ebd8
pwiener <patryk.wiener@siroccomobile.com>
2021-11-30 20:06:40 +0100

    static classmethod

--- object_oriented_programming/classmethod_and_staticmethod.py
+++ object_oriented_programming/classmethod_and_staticmethod.py
@@ -18,17 +18,27 @@ class Student(Osoba):
         wiek, stypendium = int(wiek), float(stypendium)
         if cls.czy_imie_poprawne(imie):
             return cls(imie, wiek, stypendium)
+            # return cls(imie, wiek, stypendium)
 
     @staticmethod
     def czy_imie_poprawne(imie):
-        if imie[0].isupper() and len(imie) > 3:
-            return True
-        return False
+        return imie[0].isupper() and len(imie) > 3
+
+
+class ChildStudent(Student):
+
+    @staticmethod
+    def czy_imie_poprawne(imie):
+        return True
 
 
 if __name__ == '__main__':
     stud1 = Student("Małgorzata", 32, 0)
-    stud2 = Student.stworz_ze_stringa("Maciej 21 600")
-    print(stud1)
-    print(stud2)
-    print(Student.czy_imie_poprawne("ala"))
+    # stud2 = Student.stworz_ze_stringa("Maciej 21 600")
+    # print(stud1)
+    # print(stud2)
+    print(Student.czy_imie_poprawne("Alicja"))
+    print(stud1.czy_imie_poprawne(stud1.imie))
+
+    child = ChildStudent.stworz_ze_stringa('Alicja 24 600')
+    t=0
