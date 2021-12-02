6c9a83f9c4d44bf54d4b1f6be6d1e720992ef33a
pwiener <patryk.wiener@siroccomobile.com>
2021-12-02 18:25:08 +0100

    function equivalent

--- lambda_expressions/exercises/exercise_4_products.py
+++ lambda_expressions/exercises/exercise_4_products.py
@@ -9,6 +9,7 @@ Napisz trzy funkcje korzystajace z lambda realizujace nastepujace zadania:
 """
 from dataclasses import dataclass
 from functools import reduce
+from typing import Callable
 
 
 @dataclass
@@ -36,12 +37,43 @@ EXPECTED_PRODUCT_LIST = [
 EXPECTED_TOTAL_COST = 950
 DISCOUNTED_PRICES = [350, 50, 75]
 
+
+def is_in_cart(product: Product) -> bool:
+    return product.in_cart
+
+
+def make_discount(product: Product):
+    return product.cost * 0.5
+
+
+def make_discount_extended(discount: float) -> Callable:
+    def internal_make_discount(product: Product):
+        return product.cost * discount
+
+    return internal_make_discount
+
+
+func = make_discount_extended(discount=0.25)
+print(PRODUCT_LIST[0])
+print(func(product=PRODUCT_LIST[0]))
+
+
+def add_price(acc: float, product: Product):
+    return acc + product.cost
+
+
 if __name__ == '__main__':
     cart = list(filter(lambda item: item.in_cart, PRODUCT_LIST))
+    cart = list(filter(is_in_cart, PRODUCT_LIST))
     cart = [item for item in PRODUCT_LIST if item.in_cart]
     assert cart == EXPECTED_PRODUCT_LIST
 
-    total_cost = reduce(lambda total, item: total + item.cost, cart, 0)
+    total_cost = reduce(
+        lambda total, item: total + item.cost,
+        cart,
+        0.0,
+    )
+    total_cost = reduce(add_price, cart, 0.0)
     # to nie jest equivalent!
     # jezeli zamiast dodawania mielibyśmy np. -, to zauważmy, że już by to nie działało
     # Python nie posiada equivalentu reduce
@@ -49,5 +81,7 @@ if __name__ == '__main__':
     assert total_cost == EXPECTED_TOTAL_COST
 
     discounted_prices = list(map(lambda item: item.cost * 0.5, cart))
+    discounted_prices = list(map(make_discount, cart))
+    discounted_prices = list(map(make_discount_extended(discount=0.25), cart))
     discounted_prices = [item.cost * 0.5 for item in cart]
     assert discounted_prices == DISCOUNTED_PRICES
