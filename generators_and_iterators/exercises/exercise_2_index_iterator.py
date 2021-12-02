4471ee258b7c649735a439e8426389d5840ab7c9
pwiener <patryk.wiener@siroccomobile.com>
2021-12-02 20:47:12 +0100

    iterator exercise 2 template

--- generators_and_iterators/exercises/exercise_2_index_iterator.py
+++ generators_and_iterators/exercises/exercise_2_index_iterator.py
@@ -6,7 +6,7 @@ tworzone konta.
 W tym celu uzupelnij cialo iteratora IndexIterator.
 * __iter__ zawsze zwraca instancje obiektu, czyli 'return self'
 * w __init__ zdefiniuj poczatkowa wartosc indexu - 0
-* przy kazdym nastepnym uzyciu iteratora zwracany jest indeks o jeden wiekszy niz poprzdenim razem
+* przy kazdym nastepnym uzyciu iteratora zwracany jest indeks o jeden wiekszy niz poprzednim razem
 * w __next__ musimy zwracac wartość indeksu oraz go inkrementowac, aby nastepnym razem użyć juz
   większego indeksu
 
@@ -35,6 +35,10 @@ class Account:
         self.password = password
         self.id = _id if _id else next(index_iterator)
 
+        # if _id:
+        #     self.id = _id
+        # else:
+        #     self.id = next(index_iterator)
 
 if __name__ == '__main__':
     accounts = [
@@ -47,3 +51,6 @@ if __name__ == '__main__':
     for account in accounts:
         assert account.id == expected_id
         expected_id += 1
+
+    # account1 = Account('pwiener', '123qweASD')  # id = 1
+    # account2 = Account('majster', 'alamakota')  # id = 2
